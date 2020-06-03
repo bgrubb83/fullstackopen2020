f752a079962d2381ac179557a37b30f1afd715c5
Barry Grubb <bgrubb83@gmail.com>
2020-06-03 20:56:32 +0100

    2.13.

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,21 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-function Country(props) {
-  return <p>{props.country.name}</p>
+function Country({country, showCountry}) {
+  return (
+    <div>
+      {country.name}
+      <button onClick={event => showCountry(event, country.name)}>show</button>
+    </div>
+  )
 }
 
-function CountryDetail(props) {
+function CountryDetail({country}) {
   return (
     <>
-      <h1>{props.country.name}</h1>
-      <p>capital {props.country.capital}</p>
-      <p>population {props.country.population}</p>
+      <h1>{country.name}</h1>
+      <p>capital {country.capital}</p>
+      <p>population {country.population}</p>
       <h2>Languages</h2>
       <ul>
-        {props.country.languages.map(language => <li>{language.name}</li>)}
+        {country.languages.map(language => <li key={language.name}>{language.name}</li>)}
       </ul>
-      <img height={100} src={props.country.flag} alt={`flag of ${props.country.name}`} />
+      <img height={100} src={country.flag} alt={`flag of ${country.name}`} />
     </>
   )
 }
@@ -35,13 +40,17 @@ function App() {
 
   useEffect(() => {
     const matchesFound = countries.filter(country => {
-      return country.name.toLowerCase().includes(filter);
+      return country.name.toLowerCase().includes(filter.toLowerCase());
     });
     setMatches(matchesFound);
   }, [countries, filter])
 
   const handleFilterChange = (event) => {
     setFilter(event.target.value);
+  }
+
+  const showCountry = (event, countryName) => {
+    setFilter(countryName)
   }
 
   return (
@@ -56,7 +65,7 @@ function App() {
           (matches.length > 10 ?
             'Too many matches, specify another filter'
             :
-            matches.map(match => <Country country={match} key={match.name} />)
+            matches.map(match => <Country country={match} key={match.name} showCountry={showCountry} />)
           )}
       </div>
     </form>
