a2bd9fff9031cf59fa208c0e6cb00ece0d4b9a61
Barry Grubb <bgrubb83@gmail.com>
2020-06-05 20:04:33 +0100

    2.14.

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-function Country({country, showCountry}) {
+function Country({ country, showCountry }) {
   return (
     <div>
       {country.name}
@@ -10,17 +10,43 @@ function Country({country, showCountry}) {
   )
 }
 
-function CountryDetail({country}) {
+function Weather({ weather }) {
+  return (
+    <>
+      <p>temperature: {weather.temperature} Celcius</p>
+      <img
+        height={100}
+        src={weather.weather_icons ? weather.weather_icons[0] : null}
+        alt={weather.weather_descriptions ? weather.weather_descriptions[0] : null}
+      />
+      <p>wind: {weather.wind_speed} kph direction {weather.wind_dir}</p>
+    </>
+  )
+}
+
+function CountryDetail({ country, weather, setWeather }) {
+  const apiKey = process.env.REACT_APP_API_KEY;
+  const city = country.capital;
+  useEffect(() => {
+    axios
+      .get(`http://api.weatherstack.com/current?access_key=${apiKey}&query=${city}`)
+      .then(response => {
+        setWeather(response.data.current)
+      })
+  }, [city, setWeather])
+
   return (
     <>
       <h1>{country.name}</h1>
       <p>capital {country.capital}</p>
       <p>population {country.population}</p>
-      <h2>Languages</h2>
+      <h2>Spoken languages</h2>
       <ul>
         {country.languages.map(language => <li key={language.name}>{language.name}</li>)}
       </ul>
       <img height={100} src={country.flag} alt={`flag of ${country.name}`} />
+      <h2>Weather in {country.capital}</h2>
+      <Weather weather={weather} />
     </>
   )
 }
@@ -29,6 +55,7 @@ function App() {
   const [countries, setCountries] = useState([]);
   const [filter, setFilter] = useState('');
   const [matches, setMatches] = useState([]);
+  const [weather, setWeather] = useState([]);
 
   useEffect(() => {
     axios
@@ -60,12 +87,22 @@ function App() {
       </div>
       <div>
         {matches.length === 1 ?
-          <CountryDetail country={matches[0]} />
+          <CountryDetail
+            country={matches[0]}
+            weather={weather}
+            setWeather={setWeather}
+          />
           :
           (matches.length > 10 ?
             'Too many matches, specify another filter'
             :
-            matches.map(match => <Country country={match} key={match.name} showCountry={showCountry} />)
+            matches.map(match => {
+              return <Country
+                country={match}
+                key={match.name}
+                showCountry={showCountry}
+              />
+            })
           )}
       </div>
     </form>
